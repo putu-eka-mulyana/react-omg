2854471e5d2472b49abbab0ed0fd28b5cf96bf16
Putu Eka Mulyana <putuekamulyana97@gmail.com>
2019-05-19 10:23:00 -0400

    bekerja di bagian leading

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
+import { Navbar, Nav} from 'react-bootstrap';
+class Footer extends Component {
+	render() {
+		return (
+			<footer className="footer-distributed">
+			<div className="footer-left">
+				<h3>OMG</h3>
+				<p className="footer-company-name">online market monitoring &copy; 2019</p>
+			</div>
+			<div className="footer-center">
+				<div>
+					<i className="fa fa-map-marker"></i>
+					<p>Indonesia</p>
+				</div>
+				<div>
+					<i className="fa fa-phone"></i>
+					<p>+62 555 7776 9998</p>
+				</div>
+				<div>
+					<i className="fa fa-envelope"></i>
+					<p>support@omg.com</p>
+				</div>
+			</div>
+			<div className="footer-right">
+				<p className="footer-company-about">
+					<span>About the company</span>
+					Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
+				</p>
+				<div className="footer-icons">
+					<a href="#"><i className="fa fa-facebook"></i></a>
+					<a href="#"><i className="fa fa-twitter"></i></a>
+					<a href="#"><i className="fa fa-linkedin"></i></a>
+				</div>
+			</div>
+		</footer>
+		);
+	}
+}
+export default Footer;
--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -1,60 +1,90 @@
-import React,{ Component } from "react";
+import React, { Component } from "react";
 // import { Link } from "react-router-dom";
-import { Jumbotron, Container, Row,Col,Button } from "react-bootstrap";
+import { Jumbotron, Container, Row, Col, Button, Card } from "react-bootstrap";
 // import { library } from "@fortawesome/fontawesome-svg-core";
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faChartLine } from '@fortawesome/free-solid-svg-icons';
 // library.add(faChartLine);
 import 'font-awesome/css/font-awesome.min.css';
+import Footer from "./Footer";
 
 class Landing extends Component {
-	constructor(props){
+	constructor(props) {
 		super(props);
-		this.state={
-			about_image:"https://res-staging.cloudinary.com/omg-helo/image/upload/v1556082332/trand_t1prbm.jpg",
-			img_jumbo:"https://res-staging.cloudinary.com/omg-helo/video/upload/v1556094907/Money_qe1bcn.webm"
+		this.state = {
+			about_image: "https://res-staging.cloudinary.com/omg-helo/image/upload/v1556082332/trand_t1prbm.jpg",
+			img_jumbo: "https://res-staging.cloudinary.com/omg-helo/video/upload/v1556094907/Money_qe1bcn.webm"
 		}
 	}
-	render(){
-		return(
+	render() {
+		return (
 			<div>
-			<Jumbotron className="jumbo-edit">
-				<div className="kotak_jumbo">
-					<h1 className="m-900">Lorem ipsum dolor sit amet.</h1>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium rem eligendi dolorum nam reiciendis porro vitae consectetur laudantium adipisci quaerat.</p>
-					<div className="text-center">
-						<Button variant="danger" className="b_bergabung">Bergabung Bersama kami</Button>
+				<Jumbotron className="jumbo-edit">
+					<div className="kotak_jumbo">
+						<h1 className="m-900 text-center">SELAMAT DATANG PEJUANG</h1>
+						<h3 className="text-center">MUDA</h3>
+						<div className="text-center">
+							<Button variant="danger" className="b_bergabung">JOIN NOW</Button>
+						</div>
 					</div>
-				</div>
-			</Jumbotron>
-			<section id="about">
-				<Container>
-					<h2 className="about-title text-center" id="about">ABOUT US</h2>
-					<p className="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam at facilis a facere animi aliquid quam nihil dignissimos corrupti delectus eligendi, praesentium nam perspiciatis deleniti quod, sunt tenetur! Blanditiis, tenetur!</p>
-				</Container>
-				<Container>
-					<Row>
-						<Col sm={6}><div className="img_about"></div></Col>
-						<Col sm={6}>
-							<div className="test">
-							<h5>APA MANFAATNYA YANG DIDAPATKAN ?</h5>
-							<ul className="fitur">
-								<li><i className="fa fa-crosshairs"></i> DAPATKAN TARGET YANG TEPAT</li>
-								<li><i class="fa fa-shopping-cart"></i> TINGKATKAN PENJUALAN SECARA MAKSIMAL</li>
-								<li><i class="fa fa-gift"></i> TINGKATKAN PENDAPATAN</li>
-							</ul>
-							</div>
-						</Col>
-					</Row>
-				</Container>
-				<section style={{
-					height:"900px"	
-				}}>
-
+				</Jumbotron>
+				<section id="about">
+					<Container>
+						<h2 className="about-title text-center" id="about">ABOUT US</h2>
+						<p className="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam at facilis a facere animi aliquid quam nihil dignissimos corrupti delectus eligendi, praesentium nam perspiciatis deleniti quod, sunt tenetur! Blanditiis, tenetur!</p>
+					</Container>
+					<Container>
+						<Row>
+							<Col sm={6}><div className="img_about"></div></Col>
+							<Col sm={6}>
+								<div className="test">
+									<h5>APA MANFAATNYA YANG DIDAPATKAN ?</h5>
+									<ul className="fitur">
+										<li><i className="fa fa-crosshairs"></i> DAPATKAN TARGET YANG TEPAT</li>
+										<li><i class="fa fa-shopping-cart"></i> TINGKATKAN PENJUALAN SECARA MAKSIMAL</li>
+										<li><i class="fa fa-gift"></i> TINGKATKAN PENDAPATAN</li>
+									</ul>
+								</div>
+							</Col>
+						</Row>
+					</Container>
 				</section>
-			</section>
+				<section id="fiturs" className="mt-5">
+					<Container>
+						<h1 className="text-center text-light">FITUR</h1>
+						<Row className="mt-5">
+							<Col sm={{span:2,offset:2}} className="float-right">
+								<Card style={{ width: '18rem' }}>
+									<Card.Img variant="top" src="https://res-staging.cloudinary.com/omg-helo/image/upload/v1558270282/luke-chesser-1399685-unsplash_xk6mfh.jpg" />
+									<Card.Body>
+										<Card.Title>Analisis Pasar</Card.Title>
+										<Card.Text>
+											Some quick example text to build on the card title and make up the bulk of
+											the card's content.
+    </Card.Text>
+					
+									</Card.Body>
+								</Card>
+							</Col>
+							<Col sm={{span:3,offset:3}}>
+								<Card style={{ width: '18rem' }}>
+									<Card.Img variant="top" src="https://res-staging.cloudinary.com/omg-helo/image/upload/v1558270300/mr-lee-618477-unsplash_yu0p2w.jpg" />
+									<Card.Body>
+										<Card.Title>Tren Produk</Card.Title>
+										<Card.Text>
+											Some quick example text to build on the card title and make up the bulk of
+											the card's content.
+    </Card.Text>
+		
+									</Card.Body>
+								</Card>
+							</Col>
+						</Row>
+					</Container>
+				</section>
+				<Footer/>
 			</div>
-			)
+		)
 	}
 }
 export default Landing;
